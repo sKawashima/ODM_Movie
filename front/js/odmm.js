bf659b98b93ae6de3c36405ba0e2c1a8a429b04c
sKawashima <bysk.333@gmail.com>
2018-11-15 14:48:56 +0900

    load img

--- front/js/odmm.js
+++ front/js/odmm.js
@@ -1,1 +1,13 @@
-console.log('helli')
+import sample1 from '../img/sample1.jpg'
+
+const c = document.getElementById('odmm')
+const ctx = c.getContext('2d')
+
+const img = new Image()
+img.src = sample1
+
+img.onload = () => {
+  ctx.drawImage(img, 0, 0)
+}
+
+// const draw = () => {}
